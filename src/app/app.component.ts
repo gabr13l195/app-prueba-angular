bfa6ff7c8d6dcd05fbf65fc3fd828720b104ab77
Wilmer Betancourt <wilmergabybet@gmail.com>
2025-02-19 16:39:26 -0500

    creacion de componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SaludoComponent } from './saludo/saludo.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { PeliculasComponent } from "./pages/peliculas/peliculas.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SaludoComponent, NavBarComponent],
+  imports: [RouterOutlet, SaludoComponent, NavBarComponent, PeliculasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/peliculas/peliculas.component.ts
+++ src/app/pages/peliculas/peliculas.component.ts
@@ -7,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './peliculas.component.css'
 })
 export class PeliculasComponent {
+  nombre = "Matrix";
+  anio = 1999;
+  genero = "accion"
+
+  pelicula = {
+    nombre: "Mr Robot",
+    anio: 2014,
+    genero: "drama"
+  }
 
 }
+
